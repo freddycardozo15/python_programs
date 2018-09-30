a1d2e31778455875fd4e9d2f772f6d63aa6da16d
freddycardozo15 <43716311+freddycardozo15@users.noreply.github.com>
2018-09-30 16:35:30 +0530

    Update processCmdLine.py

--- CommandLine/processCmdLine.py
+++ CommandLine/processCmdLine.py
@@ -1,4 +1,5 @@
 # This python program processes the command line arguments using a json file.
+# Any new command line arguments need to be added to the json file 
 # The command parameters are defined in the json file commandLine.json 
 
 __author__ = 'Freddy Cardozo'
@@ -8,13 +9,17 @@ from pprint import pprint
 
 from common import readJsonFile
 from common import processCommandLineArgs
+from common import ProgramError
 
 try:
-    # Read the command line argument fields from the json file 'commandLineArguments.json'
-    cmd_line_hash = readJsonFile('commandLineArguments.json')
-    pprint(cmd_line_hash)
-    cmd_line_obj  = processCommandLineArgs(cmd_line_hash)
-except StopIteration as e:
-    print("Stop Iteration  occured")
-except:
-    print("Error Ocured")
+    try:
+        # Read the command line argument fields from the json file 'commandLineArguments.json'
+        cmd_line_hash = readJsonFile('commandLineArguments.json')
+        pprint(cmd_line_hash)
+        cmd_line_obj  = processCommandLineArgs(cmd_line_hash)
+    except StopIteration as e:
+        print("Stop Iteration occured")
+    except Exception as e:
+        raise ProgramError(e)
+except ProgramError as pe:
+    print ("Program Error\n")
