6251bfb8511334eddb02b18176fbcab67cf368d1
freddycardozo15 <43716311+freddycardozo15@users.noreply.github.com>
2018-09-30 15:42:21 +0530

    Update common.py

--- CommandLine/common.py
+++ CommandLine/common.py
@@ -1,4 +1,5 @@
-import argparse
+from argparse    import ArgumentParser
+from json        import load as jsonLoad
 from collections import OrderedDict as od
 
 # Description :   Process command line arguments using argparse python module
@@ -7,17 +8,32 @@ from collections import OrderedDict as od
 # Return Value:
 #    cmdLineObj  : Argument Parser object having all the parsed objects
 def processCommandLineArgs(cmdLineDict=od()):
-    argParseObj = argparse.ArgumentParser(description="Parse the command line arguments")
-    for option in cmdLineDict:
-        subOptionArr = list()
-        for optionArgs, optionArgValue in cmdLineDict[option].items():
-            if type(optionArgValue) is str or type(optionArgValue) is unicode:
-                subOptionArr.append("{}='{}'".format(subOption, str(optionArgValue)))
-            else:
-                subOptionArr.append("{}={}".format(subOption, str(optionArgValue)))
-        subOptionStr = ','.join(subOptionArr)
-        cmdLineStr = "'" +  option + "'" + ',' + subOptionStr
-        cmdLineStr = "argParseObj.add_argument({})".format(cmdLineStr)
-        ret = eval(cmdLineStr)
+    argParseObj = ArgumentParser(description="Parse the command line arguments")
+    try:
+        for option in cmdLineDict.keys():
+            subOptionArr = list()
+            for optionArg, optionArgValue in cmdLineDict[option].items():
+                if type(optionArgValue) is str:
+                    subOptionArr.append("{}='{}'".format(optionArg, str(optionArgValue)))
+                else:
+                    subOptionArr.append("{}={}".format(optionArg, str(optionArgValue)))
+            subOptionStr = ','.join(subOptionArr)
+            cmdLineStr   = "'" +  option + "'" + ',' + subOptionStr
+            cmdLineStr   = "argParseObj.add_argument({})".format(cmdLineStr)
+            returnVal    = eval(cmdLineStr)
+            if returnVal == 0:
+                print("add_arguement worked fine \n")
+    except StopIteration:
+        pass
     cmdLineObj = argParseObj.parse_args()
     return cmdLineObj
+
+
+# Description :   Reads a json file and forms a dictionary
+# Input Arguments:
+#           cmdLineDict : Dictionary having all the options being passed
+# Return Value:
+#            Argument Parser object having all the parsed objects
+def readJsonFile(jsonFile):
+    jsonData = od(jsonLoad(open(jsonFile, mode='r')))
+    return jsonData
