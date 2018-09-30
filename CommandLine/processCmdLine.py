3429363b81189c35c305f1e1873cbb2f16b88162
freddycardozo15 <43716311+freddycardozo15@users.noreply.github.com>
2018-09-30 12:07:22 +0530

    Adding some comments

--- CommandLine/processCmdLine.py
+++ CommandLine/processCmdLine.py
@@ -1,8 +1,11 @@
 # This python program processes the command line arguments using a json file.
 # The command parameters are defined in the json file commandLine.json 
 
+__author__ = 'Freddy Cardozo'
+
 try:
-  cmdLineDict = readJsonFile('commandLineARguments.json')
+  # Read the command line argument fields from the json file 'commandLineArguments.json'
+  cmdLineDict = readJsonFile('commandLineArguments.json')
   cmdLineObj  = processCommandLineArgs(cmdLineDict)
 except:
   print "Error occured"
