4c3f2b27d177bd0626a13d93446022a527330051
freddycardozo15 <43716311+freddycardozo15@users.noreply.github.com>
2018-09-30 15:41:09 +0530

    Update processCmdLine.py

--- CommandLine/processCmdLine.py
+++ CommandLine/processCmdLine.py
@@ -3,9 +3,18 @@
 
 __author__ = 'Freddy Cardozo'
 
+from pdb    import set_trace as st
+from pprint import pprint 
+
+from common import readJsonFile
+from common import processCommandLineArgs
+
 try:
-  # Read the command line argument fields from the json file 'commandLineArguments.json'
-  cmdLineDict = readJsonFile('commandLineArguments.json')
-  cmdLineObj  = processCommandLineArgs(cmdLineDict)
+    # Read the command line argument fields from the json file 'commandLineArguments.json'
+    cmd_line_hash = readJsonFile('commandLineArguments.json')
+    pprint(cmd_line_hash)
+    cmd_line_obj  = processCommandLineArgs(cmd_line_hash)
+except StopIteration as e:
+    print("Stop Iteration  occured")
 except:
-  print "Error occured"
+    print("Error Ocured")
